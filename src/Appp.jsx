15b6543c1b9ad5d48d4a5ad4a1bca4e99cffc0e1
Syahrezaimamw <syahrezaimamwahyudi976@gmail.com>
2024-01-19 06:39:37 +0700

    initial commit

--- src/Appp.jsx
+++ src/Appp.jsx
@@ -21,7 +21,7 @@ const router=createBrowserRouter([
      <Routes >
         <Route exact path="/tugass" element={<Log/>} />
         <Route exact path="/tugass/login" element={<Login/>} />
-        <Route exact path="/sec" element={<Sec/>} />
+        <Route exact path="/tugass/sec" element={<Sec/>} />
         <Route exact path='/*' element={<Eror/>} />
         </Routes>
 
--- src/Log.jsx
+++ src/Log.jsx
@@ -4,6 +4,7 @@ export default function Log() {
   return (
     <div>
         <Link to='/tugass/login'>klikk</Link>
+        <Link to='/tugass/sec'>klikk</Link>
     </div>
   )
 }
